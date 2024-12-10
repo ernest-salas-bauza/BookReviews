80fcf1d7ad6b6ea40029149e8cf7dc4347be8f57
esalasbauza <esalasbauza@gmail.com>
2024-12-10 17:06:06 +0100

    Create seed reviews associated with books

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\Review;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,22 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Book::factory(33)->create()->each(function ($book) {
+
+            Review::factory(0)->count(6)->good()->for($book)->create();
+        });
+        Book::factory(33)->create()->each(function ($book) {
+
+            Review::factory(0)->count(25)->average()->for($book)->create();
+        });
+        Book::factory(34)->create()->each(function ($book) {
+
+            Review::factory(0)->count(15)->bad()->for($book)->create();
+        });
     }
 }
